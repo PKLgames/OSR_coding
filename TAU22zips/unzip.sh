d904e2cdefb4a9d94163b99c9820e09e5f64bfbc
PKLgames <PKLgames@163.com>
2026-03-12 11:25:56 +0000

    unzip.sh

--- TAU22zips/unzip.sh
+++ TAU22zips/unzip.sh
@@ -2,14 +2,17 @@
 
 # 设置变量
 SOURCE_DIR="."  # 压缩文件所在目录，默认为当前目录
-TARGET_DIR="../TAU22"  # 统一存放wav文件的目标文件夹
+TARGET_DIR="../TAU-urban-acoustic-scenes-2022-mobile-development/audio"  # 统一存放wav文件的目标文件夹
 EXTRACT_DIR="../temp_extract"  # 临时解压目录
+
+echo "开始处理TAU-urban-acoustic-scenes-2022-mobile-development压缩文件..."
+
+unzip -q -o ./TAU-urban-acoustic-scenes-2022-mobile-development.doc.zip -d ../
+unzip -q -o ./TAU-urban-acoustic-scenes-2022-mobile-development.meta.zip -d ../
 
 # 创建目标文件夹和临时目录
 mkdir -p "$TARGET_DIR"
 mkdir -p "$EXTRACT_DIR"
-
-echo "开始处理TAU-urban-acoustic-scenes-2022-mobile-development压缩文件..."
 
 # 查找并排序所有相关的压缩文件
 # 使用ls按数字顺序排序，确保按正确顺序解压
